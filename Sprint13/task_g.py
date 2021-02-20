011d3e80b43c5716f02869972ce689039aaaa3aa
Vitalij Holodov <w.a.holodov@yandex.ru>
2021-02-20 15:54:55 +0500

    Sprint 13. Task F

--- Sprint13/task_g.py
+++ Sprint13/task_g.py
@@ -1,18 +1,29 @@
-class StackMax():
+class StackMaxEffective():
     def __init__(self):
         self.items = []
+        self.max_item = []
 
     def push(self, item):
         self.items.append(item)
+        if self.max_item:
+            if item >= self.max_item[-1]:
+                self.max_item.append(item)
+        else:
+            self.max_item.append(item)
 
     def pop(self):
         try:
-            self.items.pop()
+            item = self.items.pop()
+            if item == self.max_item[-1]:
+                self.max_item.pop()
         except IndexError:
             print('error')
 
     def get_max(self):
-        print(max(self.items, default='None'))
+        if self.max_item:
+            print(self.max_item[-1])
+        else:
+            print('None')
 
 
 def metod_stackmax(stackmax, command):
@@ -26,7 +37,7 @@ def metod_stackmax(stackmax, command):
 
 
 if __name__ == '__main__':
-    stackmax = StackMax()
+    stackmax = StackMaxEffective()
     n = int(input())
     for i in range(n):
         metod_stackmax(stackmax, input())
