59b29432cad942d489a5064509bb4a89c7b25d9f
Vitalij Holodov <w.a.holodov@yandex.ru>
2021-03-04 20:26:37 +0500

    WIP

--- Sprint14/task_h.py
+++ Sprint14/task_h.py
@@ -1,8 +1,10 @@
 def first_digit(nums):
     key = [x for x in nums]
     if len(key) < 4:
-        for _ in range(4 - len(key)):
-            key.append(key[-1])
+        admix = key[:(4 - len(key))]
+        while len(key) != 4:
+            for dig in admix:
+                key.append(dig)
     return key
 
 
