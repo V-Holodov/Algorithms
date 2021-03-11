152c79c6377b7406666288a8ff512baa0999c8c8
Vitalij Holodov <w.a.holodov@yandex.ru>
2021-03-11 18:46:03 +0500

    task D

--- Sprint14/task_d.py
+++ Sprint14/task_d.py
@@ -1,17 +1,14 @@
 def feed_children(children, cookies):
-    cookies.sort()
-    children.sort()
+    children.sort(reverse=True)
+    cookies.sort(reverse=True)
     result = 0
-    for child in children:
-        flag = True
-        for cook in cookies:
-            if cook >= child:
-                result += 1
-                cookies.remove[cook]
-                flag = False
+    cook = 0
+    for i in range(len(children)):
+        if children[i] <= cookies[cook]:
+            result += 1
+            cook += 1
+            if cook >= len(cookies):
                 break
-        if flag:
-            break
     print(result)
 
 
